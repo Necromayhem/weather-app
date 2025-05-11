24c9db831c6555d672ddfe9650bb247c05a8a27f
Necromayhem <aleksanderkuklev1488@gmail.com>
2025-05-11 13:38:32 +0300

    add .env

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,2 +1,2 @@
-export const API_KEY = '95bd7f05c4d243c519a9a29a0bbbcc10';
-export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+export const API_KEY = import.meta.env.VITE_API_KEY;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
